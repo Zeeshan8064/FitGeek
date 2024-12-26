62f5cfdeb24861b94fe724b71f7fad4a1b0ea9c8
Zeeshan Ahmad <99203352+Zeeshan8064@users.noreply.github.com>
2024-12-26 17:58:18 +0100

    Update HomeBanner2.tsx

--- src/components/HomeBanner2/HomeBanner2.tsx
+++ src/components/HomeBanner2/HomeBanner2.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { Suspense } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import './HomeBanner2.css'
+import './HomeBanner2.css';
 import { useSearchParams } from 'next/navigation';
 
 // Import Swiper styles
@@ -11,80 +11,87 @@ import 'swiper/css/navigation';
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-
 const HomeBanner2 = () => {
-  const [workouts, setWorkouts] = React.useState<any[] | null>(null)
-  const [data, setData] = React.useState<any[] | null>(null)
-  const searchParams = useSearchParams()
-  const workoutid = searchParams.get('id')
+  const [workouts, setWorkouts] = React.useState<any[] | null>(null);
+  const [data, setData] = React.useState<any[] | null>(null);
+  const searchParams = useSearchParams();
+  const workoutid = searchParams.get('id');
 
   const getData = async () => {
-    fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/workoutplans/workouts',{
+    fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/workoutplans/workouts', {
       method: 'GET',
       credentials: 'include',
     })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      if (data.ok){
-        setData(data.data)
-      }else{
-        setData([])
-      }
-    })
-    .catch(err =>
-      {console.log('Error:', err)
-        setData([])
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.ok) {
+          setData(data.data);
+        } else {
+          setData([]);
+        }
       })
-  }
+      .catch((err) => {
+        console.log('Error:', err);
+        setData([]);
+      });
+  };
+
   React.useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
+
   return (
     <>
-    <div>
-      <h1 className='mainhead1'>Workouts</h1>
-      <Swiper
-      slidesPerView={3}
-        spaceBetween={30}
-        centeredSlides={false}
-        autoplay={{
-          delay: 3500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        {
-          data && data.map((item, index) => {
-            return (
-              <SwiperSlide key={index} >
-                <div className='swiper-slide'
-                  style={{
-                    backgroundImage: `url(${item.imageURL})`,
-                  }}
-                  onClick={() => {
-                    window.location.href = `/workout/${item._id}`
-                  }}
-                >
-                  <div className='swiper-slide-content'>
-                    <h2>{item.name}</h2>
-                    <p>{item.durationInMinutes} min</p>
+      <div>
+        <h1 className="mainhead1">Workouts</h1>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          centeredSlides={false}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {data &&
+            data.map((item, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <div
+                    className="swiper-slide"
+                    style={{
+                      backgroundImage: `url(${item.imageURL})`,
+                    }}
+                    onClick={() => {
+                      window.location.href = `/workout/${item._id}`;
+                    }}
+                  >
+                    <div className="swiper-slide-content">
+                      <h2>{item.name}</h2>
+                      <p>{item.durationInMinutes} min</p>
+                    </div>
                   </div>
-                </div>
-              </SwiperSlide>
-            )
-          })
-        }
-
-      </Swiper>
-    </div>
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HomeBanner2
+// Wrap in Suspense
+export default function SuspendedHomeBanner() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeBanner2 />
+    </Suspense>
+  );
+}
